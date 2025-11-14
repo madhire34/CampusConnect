868d9dd9a6abfe1e17b06d334c5e7707f7c74912
mahanandi reddy <madhirereddy4@gmail.com>
2025-11-14 06:20:39 +0530

    Use environment variables for SECRET_KEY and DEBUG

--- campusorbit/settings.py
+++ campusorbit/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m5=b1g_t2pm5yy(4mf@awg$vc2t7@0+w4=6k4nhl&i=zkyydxe'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-m5=b1g_t2pm5yy(4mf@awg$vc2t7@0+w4=6k4nhl&i=zkyydxe')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False').lower() == 'true'
 
 
 ALLOWED_HOSTS = ["*"]
